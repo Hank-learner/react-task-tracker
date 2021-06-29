1331f0460425c1d5c555f0619d97055721e75f8c
Hank-learner <jerryrocker6@gmail.com>
2021-06-29 16:00:36 +0530

    test cases

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,29 +1,6 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from '../App';
 
-
-// test('renders', () => {
-//     const root = document.createElement('div');
-
-//     ReactDom.render(<App />, root)
-
-//     expect(root.querySelector('header h1').textContent).toBe(
-//         "Task Tracker"
-//     );
-
-    
-//     const {getByText, getByLabelText} = getQueriesForElement(root); 
-
-//     //check if heading present or not
-//     expect(getByText('Task Tracker')).not.toBeNull();
-
-//     //form check label matches the feild id (htmlFor)
-//     // expect(getByLabelText('Enter Task')).not.toBeNull();
-
-//     ReactDom.unmountComponentAtNode(root);
-// });
-
-
 describe("Main page snapshot", () => {
     it('should take a snapshot', () => {
         const { asFragment } = render(<App />)
@@ -53,3 +30,24 @@ describe("rendering", () => {
     });
 })
 
+
+// test('renders', () => {
+//     const root = document.createElement('div');
+
+//     ReactDom.render(<App />, root)
+
+//     expect(root.querySelector('header h1').textContent).toBe(
+//         "Task Tracker"
+//     );
+
+
+//     const {getByText, getByLabelText} = getQueriesForElement(root); 
+
+//     //check if heading present or not
+//     expect(getByText('Task Tracker')).not.toBeNull();
+
+//     //form check label matches the feild id (htmlFor)
+//     // expect(getByLabelText('Enter Task')).not.toBeNull();
+
+//     ReactDom.unmountComponentAtNode(root);
+// });
